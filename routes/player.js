b152b4f588e74be75be1b015ba1ce71115575489
firllyw <fwahyudi@gmail.com>
2020-09-20 22:03:39 +0700

    rework

--- routes/player.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-var controller = require('../controller/player')
-
-router.route('/').get(controller.index).post(controller.add)
-
-
-
-module.exports = router;
